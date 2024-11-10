2ba9c4f1d06383e641e27362cbdc25e841fa236a
Vitalii.Marsh <kaarido@yandex.ru>
2024-11-10 17:59:04 +0300

    add format

--- gendiff.js
+++ gendiff.js
@@ -7,4 +7,6 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv);
